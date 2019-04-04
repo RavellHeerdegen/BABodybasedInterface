b8b2df17e163962f805716887f8e633bc15143db
Heerscher <ravell1993@yahoo.de>
2019-04-04 18:16:46 +0200

    Button Events Test
    SteamVR eingebettet, Player erzeugt, Raum erzeugt, Kameraan Headset angepasst, CreateButton mit ClickEvent, ButtonEventsObject für EventScripte

---
+++ Assets/Scripts/ButtonClickEvents.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickEvents : MonoBehaviour
+{
+    public void buttonOneClick() {
+        Debug.Log("button one has been clicked");
+    }
+}
