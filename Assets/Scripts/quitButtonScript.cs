edf7e80035b75450f89c4ac88647bb0997c8901f
VRLab PC <>
2019-05-09 16:29:05 +0200

    Working Evaluation Build
    Fixed calibration, head position fix, rotation fix, Quit button for exit, scale fix

---
+++ Assets/Scripts/quitButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitButtonScript : MonoBehaviour
+{
+
+    // Handles the click event of the Button
+    public void ButtonClicked()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        //Application.Quit();
+    }
+}
